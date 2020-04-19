dcbba38774cfc8a682d122772458326673a60556
Andy Dunstall <andydunstall@hotmail.com>
2020-04-19 04:24:11 -0400

    Update structure

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -3,10 +3,10 @@ pub mod producer;
 
 #[cfg(test)]
 mod tests {
-    #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+    #[test]
     fn it_works_2() {
         assert_eq!(2 + 2, 4);
     }
