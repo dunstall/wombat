c1e611900c263415676aac7dc7a6c99baa86e48b
Andy Dunstall <andydunstall@hotmail.com>
2020-04-30 02:33:26 -0400

    Split len trait from segment

--- broker/src/log/segment.rs
+++ broker/src/log/segment.rs
@@ -3,10 +3,17 @@ use crate::log::result::Result;
 
 use async_trait::async_trait;
 
+pub trait Len {
+    fn len(&self) -> u64;
+
+    fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+}
+
 #[async_trait]
-pub trait Segment {
+pub trait Segment: Len {
     async fn open(dir: &str, path: &str) -> Self;
     async fn append(&mut self, log: Log) -> Result<u64>;
     async fn lookup(&mut self, offset: u64) -> Result<Log>;
-    async fn size(&mut self) -> Result<u64>;
 }
