cb7af758bad52a762e430557ebf6df4b08155d37
Andy Dunstall <andydunstall@hotmail.com>
2020-05-02 04:45:19 -0400

    Remove client

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,14 +1,7 @@
-pub mod connection;
-pub mod consumer;
-pub mod producer;
-
 #[cfg(test)]
 mod tests {
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
     #[test]
-    fn it_works_2() {
+    fn it_works() {
         assert_eq!(2 + 2, 4);
     }
 }
