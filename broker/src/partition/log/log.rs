bd621739767fd188583d5a33ae43a291721adf83
Andy Dunstall <andydunstall@hotmail.com>
2020-05-03 10:12:20 -0400

    Update tdd tests

--- broker/src/partition/log/log.rs
+++ broker/src/partition/log/log.rs
@@ -5,14 +5,13 @@ use tokio::fs::{File, OpenOptions};
 
 use crate::partition::log::{LogResult, Record};
 
-// Not thread safe as read and write in multiple chunks so may interleave.
 pub struct Log {
     file: File,
 }
 
 // TODO(AD) Multi segment and expiry
 impl Log {
-    pub async fn open(dir: &Path, max_segment_size: u64) -> LogResult<Log> {
+    pub async fn open(dir: &Path) -> LogResult<Log> {
         fs::create_dir_all(dir).await?;
         let file = OpenOptions::new()
             .append(true)
@@ -33,90 +32,4 @@ impl Log {
         self.file.seek(SeekFrom::Start(offset)).await?;
         Record::read_from(&mut self.file).await
     }
-
-    // pub async fn expire(since) TODO
-}
-
-#[cfg(test)]
-mod tests {
-    use tempdir::TempDir;
-
-    use super::*;
-
-    #[tokio::test]
-    async fn open_dir_not_exist() {
-        let dir = TempDir::new("wombatlog").unwrap();
-        let not_exist_path = dir.path().join("does_not_exist");
-        let mut log = Log::open(&not_exist_path, 0).await.unwrap();
-
-        let record = Record::new(b"testkey".to_vec(), b"testval".to_vec());
-        assert_eq!(0, log.append(record).await.unwrap());
-        assert_eq!(
-            Record::new(b"testkey".to_vec(), b"testval".to_vec()),
-            log.lookup(0).await.unwrap()
-        );
-    }
-
-    #[tokio::test]
-    async fn open_dir_empty() {
-        let dir = TempDir::new("wombatlog").unwrap();
-        let mut log = Log::open(&dir.path(), 0).await.unwrap();
-
-        let record = Record::new(b"testkey".to_vec(), b"testval".to_vec());
-        assert_eq!(0, log.append(record).await.unwrap());
-        assert_eq!(
-            Record::new(b"testkey".to_vec(), b"testval".to_vec()),
-            log.lookup(0).await.unwrap()
-        );
-    }
-
-    #[tokio::test]
-    async fn open_existing_log() {
-        let dir = TempDir::new("wombatlog").unwrap();
-        let mut log = Log::open(&dir.path(), 2000).await.unwrap();
-
-        for offset in (0..1000).step_by(37) {
-            // Ensure each record is unique.
-            let val = format!("{:0>10}", offset.to_string());
-            let record = Record::new(b"testkey".to_vec(), val.as_bytes().to_vec());
-
-            assert_eq!(offset, log.append(record).await.unwrap());
-        }
-
-        // Create a new log object and read the values written before.
-        let mut log = Log::open(&dir.path(), 2000).await.unwrap();
-        for offset in (0..1000).step_by(37) {
-            // Ensure each record is unique.
-            let val = format!("{:0>10}", offset.to_string());
-            let record = Record::new(b"testkey".to_vec(), val.as_bytes().to_vec());
-
-            assert_eq!(record, log.lookup(offset).await.unwrap());
-        }
-    }
-
-    #[tokio::test]
-    async fn lookup_random() {
-        let dir = TempDir::new("wombatlog").unwrap();
-        let mut log = Log::open(&dir.path(), 2000).await.unwrap();
-
-        for offset in (0..1000).step_by(37) {
-            // Ensure each record is unique.
-            let val = format!("{:0>10}", offset.to_string());
-            let record = Record::new(b"testkey".to_vec(), val.as_bytes().to_vec());
-
-            assert_eq!(offset, log.append(record).await.unwrap());
-        }
-
-        let offset = 370;
-        let val = format!("{:0>10}", offset.to_string());
-        let record = Record::new(b"testkey".to_vec(), val.as_bytes().to_vec());
-
-        assert_eq!(record, log.lookup(offset).await.unwrap());
-    }
-
-    // TODO Multi segment
-
-    // TODO Expire old segment
-
-    // TODO Corrupt log
 }
--- broker/src/partition/partition.rs
+++ broker/src/partition/partition.rs
@@ -8,10 +8,12 @@ pub struct Partition {
 }
 
 impl Partition {
+    // TODO(AD) Remove unwrap() and clone()
+
     pub async fn open(dir: &Path, topic: &str, n: u32) -> Partition {
         let path = dir.join(topic).join(format!("partition-{}", n));
         Partition {
-            log: Log::open(&path, 0).await.unwrap(),
+            log: Log::open(&path).await.unwrap(),
         }
     }
 
@@ -48,12 +50,39 @@ mod tests {
     async fn open_new() {
         let dir = TempDir::new("wombatlog").unwrap();
         let not_exist_path = dir.path().join("does_not_exist");
-        let mut partition = Partition::open(&not_exist_path, "mytopic", 0).await;
 
-        put_and_get(&mut partition).await;
+        for n in 0..5 {
+            let mut partition = Partition::open(&not_exist_path, "mytopic", n).await;
+            put_and_get(&mut partition).await;
+        }
     }
 
+    #[tokio::test]
+    async fn open_existing() {
+        let dir = TempDir::new("wombatlog").unwrap();
+        let not_exist_path = dir.path().join("does_not_exist");
+
+        for n in 0..5 {
+            let mut partition = Partition::open(&not_exist_path, "mytopic", n).await;
+
+            let mut written = put(&mut partition).await;
+
+            // Recreate the partition and load.
+            let mut partition = Partition::open(&not_exist_path, "mytopic", n).await;
+            get(&mut partition, &mut written).await;
+        }
+    }
+
+    // TODO Test expire old segments
+    // TODO Test corrupt log
+
     async fn put_and_get(partition: &mut Partition) {
+        let mut written = put(partition).await;
+
+        get(partition, &mut written).await;
+    }
+
+    async fn put(partition: &mut Partition) -> Vec<(Vec<u8>, Vec<u8>)> {
         let mut rng = rand::thread_rng();
 
         let mut written = Vec::<(Vec<u8>, Vec<u8>)>::new();
@@ -69,6 +98,10 @@ mod tests {
             written.push((key, val));
         }
 
+        written
+    }
+
+    async fn get(partition: &mut Partition, written: &Vec<(Vec<u8>, Vec<u8>)>) {
         let mut offset: u64 = 0;
         for (key, val) in written.iter() {
             let (next_offset, key_read, val_read) = partition.get(offset).await;
@@ -77,6 +110,4 @@ mod tests {
             offset = next_offset;
         }
     }
-
-    // TODO
 }
