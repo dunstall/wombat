ebdb6d03d16c4b55a82556f1c6ffd20e2ce1f5fa
Andy Dunstall <andydunstall@hotmail.com>
2020-05-03 06:19:02 -0400

    Add key and val to consume resp

--- common/src/packet/consumeresponse.rs
+++ common/src/packet/consumeresponse.rs
@@ -2,20 +2,23 @@ use std::marker::Unpin;
 use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 use crate::packet::result::MessageResult;
+use crate::packet::utils;
 
 #[derive(Debug, std::cmp::PartialEq)]
 pub struct ConsumeResponse {
     offset: u64,
     next_offset: u64,
-    payload: Vec<u8>,
+    key: Vec<u8>,
+    val: Vec<u8>,
 }
 
 impl ConsumeResponse {
-    pub fn new(offset: u64, next_offset: u64, payload: Vec<u8>) -> ConsumeResponse {
+    pub fn new(offset: u64, next_offset: u64, key: Vec<u8>, val: Vec<u8>) -> ConsumeResponse {
         ConsumeResponse {
             offset,
             next_offset,
-            payload,
+            key,
+            val,
         }
     }
 
@@ -27,30 +30,31 @@ impl ConsumeResponse {
         self.next_offset
     }
 
-    pub fn payload(&self) -> &Vec<u8> {
-        &self.payload
+    pub fn key(&self) -> &Vec<u8> {
+        &self.key
+    }
+
+    pub fn val(&self) -> &Vec<u8> {
+        &self.val
     }
 
     pub async fn read_from(
         reader: &mut (impl AsyncRead + Unpin),
     ) -> MessageResult<ConsumeResponse> {
-        let payload_len = reader.read_u64().await?;
-        let offset = reader.read_u64().await?;
-        let next_offset = reader.read_u64().await?;
-        let mut payload = vec![0; payload_len as usize];
-        reader.read_exact(&mut payload).await?;
         Ok(ConsumeResponse {
-            offset,
-            next_offset,
-            payload,
+            offset: reader.read_u64().await?,
+            next_offset: reader.read_u64().await?,
+
+            key: utils::read_var(reader).await?,
+            val: utils::read_var(reader).await?,
         })
     }
 
     pub async fn write_to(&self, writer: &mut (impl AsyncWrite + Unpin)) -> MessageResult<()> {
-        writer.write_u64(self.payload.len() as u64).await?;
         writer.write_u64(self.offset).await?;
         writer.write_u64(self.next_offset as u64).await?;
-        writer.write_all(&self.payload).await?;
+        utils::write_var(&self.key, writer).await?;
+        utils::write_var(&self.val, writer).await?;
         Ok(())
     }
 }
@@ -63,25 +67,29 @@ mod tests {
 
     #[tokio::test]
     async fn write_to() {
-        let mut buf = Cursor::new(vec![0; 28]);
-        let h = ConsumeResponse::new(0xfffa, 0xffff, vec![1, 2, 3, 4]);
+        let mut buf = Cursor::new(vec![0; 40]);
+        let h = ConsumeResponse::new(0xfffa, 0xffff, vec![1, 2, 3, 4], vec![1, 2, 3, 4]);
         h.write_to(&mut buf).await.unwrap();
         assert_eq!(
-            &buf.get_ref()[0..28],
-            &[
-                0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0xff, 0xfa, 0, 0, 0, 0, 0, 0, 0xff, 0xff,
-                1, 2, 3, 4
+            &buf.get_ref()[0..40],
+            vec![
+                0, 0, 0, 0, 0, 0, 255, 250, 0, 0, 0, 0, 0, 0, 255, 255, 0, 0, 0, 0, 0, 0, 0, 4, 1,
+                2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 4, 1, 2, 3, 4
             ]
+            .as_slice()
         );
     }
 
     #[tokio::test]
     async fn read_from() {
         let mut buf = Cursor::new(vec![
-            0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0xaa, 0x12, 0, 0, 0, 0, 0, 0, 0xab, 0x4, 1,
-            2, 3, 4,
+            0, 0, 0, 0, 0, 0, 255, 250, 0, 0, 0, 0, 0, 0, 255, 255, 0, 0, 0, 0, 0, 0, 0, 4, 1, 2,
+            3, 4, 0, 0, 0, 0, 0, 0, 0, 4, 1, 2, 3, 4,
         ]);
         let r = ConsumeResponse::read_from(&mut buf).await.unwrap();
-        assert_eq!(ConsumeResponse::new(0xaa12, 0xab04, vec![1, 2, 3, 4]), r);
+        assert_eq!(
+            ConsumeResponse::new(0xfffa, 0xffff, vec![1, 2, 3, 4], vec![1, 2, 3, 4]),
+            r
+        );
     }
 }
