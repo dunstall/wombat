ef770795755f5788ae9811b0b3b9e0c7de9b4371
Andy Dunstall <andydunstall@hotmail.com>
2020-05-04 23:21:03 -0400

    Fix producer example

--- client/examples/producer.rs
+++ client/examples/producer.rs
@@ -3,9 +3,9 @@ use wombatcore::ProduceRecord;
 
 #[tokio::main]
 async fn main() {
-    let mut producer = Producer::new("localhost:3110").await;
+    let mut producer = Producer::new("localhost:3110").await.unwrap();
     loop {
         let record = ProduceRecord::new("mytopic", vec![], b"test-val".to_vec());
-        producer.send(record).await;
+        producer.send(record).await.unwrap();
     }
 }
