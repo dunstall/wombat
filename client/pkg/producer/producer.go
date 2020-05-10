54092cce54429860f45797ddfc04d179d0ca81c1
Andy Dunstall <andydunstall@hotmail.com>
2020-05-10 07:06:30 -0400

    Add basic producer

--- client/pkg/producer/producer.go
+++ client/pkg/producer/producer.go
@@ -1,15 +1,41 @@
 package producer
 
 import (
-  "github.com/dunstall/wombatclient/pkg/record"
+	"github.com/dunstall/wombatclient/pkg/record"
 )
 
-type Producer struct {}
-
-func (p *Producer) New(server string) Producer {
-	return Producer{}
+type Producer struct {
+	lb   loadBalancer
+	conn connection
 }
 
-func (p *Producer) send(record record.ProduceRecord) error {
-	return nil
+func New(addr string) (Producer, error) {
+	conn, err := connect(addr)
+	if err != nil {
+		return Producer{}, err
+	}
+	return Producer{
+		lb:   newLoadBalancer(),
+		conn: conn,
+	}, nil
+}
+
+func (p *Producer) Send(record record.ProduceRecord) error {
+	p.updatePartition(&record)
+
+	// TODO(AD) For now have a single connection. Actually need one per
+	// partition/topic.
+
+	// TODO(AD) Ack
+	return p.conn.send(record)
+}
+
+func (p *Producer) updatePartition(record *record.ProduceRecord) {
+	if record.Partition() == 0 {
+		if len(record.Key()) == 0 {
+			record.SetPartition(p.lb.nextPartition(record.Topic()))
+		} else {
+			record.SetPartition(p.lb.partitonFromKey(record.Key()))
+		}
+	}
 }
