b38522a891aec6d877e8ef075abb22c4cf23a5a9
Andy Dunstall <andydunstall@hotmail.com>
2020-05-10 10:18:08 -0400

    Add consumer offset registry in zookeeper

--- client/pkg/consumer/consumer.go
+++ client/pkg/consumer/consumer.go
@@ -1,30 +1,51 @@
 package consumer
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/dunstall/wombatclient/pkg/record"
 )
 
 type Consumer struct {
-	conn connection
+	conn    connection
+	offsets offsets
 }
 
-func New(addr string) (Consumer, error) {
-	conn, err := connect(addr)
+func New(broker string, zkServers []string, sessionTimeout time.Duration) (Consumer, error) {
+	conn, err := connect(broker)
 	if err != nil {
 		return Consumer{}, err
 	}
+
+	offsets, err := newOffsets(zkServers, sessionTimeout)
+	if err != nil {
+		return Consumer{}, err
+	}
+
 	return Consumer{
-		conn: conn,
+		conn,
+		offsets,
 	}, nil
 }
 
-func (c *Consumer) Poll(request record.ConsumeRequest) (record.ConsumeRecord, error) {
+// TODO(AD) Poll should not take arguments - distribute partitions automatically
+func (c *Consumer) Poll(partition Partition) (record.ConsumeRecord, error) {
 	// TODO(AD) Request in background on poll - should assign partitions with client
 	// coordination.
 
-	if err := c.conn.send(request); err != nil {
+	offset, err := c.offsets.lookup(partition)
+	if err != nil {
 		return record.ConsumeRecord{}, err
 	}
 
+	request := record.NewConsumeRequest(partition.topic, partition.n, offset)
+	if err := c.conn.send(request); err != nil {
+		return record.ConsumeRecord{}, err
+	}
 	return c.conn.receive()
+}
+
+func (c *Consumer) Commit(record record.ConsumeRecord, partition Partition) error {
+	return c.offsets.commit(partition, record.NextOffset())
 }
