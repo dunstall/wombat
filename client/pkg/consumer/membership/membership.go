0830b6a670b7be22ea1e31c936317692927fe186
Andy Dunstall <andydunstall@hotmail.com>
2020-05-16 02:17:00 -0400

    Add membership rebalance

--- client/pkg/consumer/membership/membership.go
+++ client/pkg/consumer/membership/membership.go
@@ -2,13 +2,21 @@ package membership
 
 import (
 	"path"
+	"sort"
+	"strconv"
 
 	"github.com/dunstall/wombatclient/pkg/consumer/registry"
 )
 
+const (
+	nPartitions = 15
+)
+
 type Membership struct {
+	id       string
 	group    string
 	registry registry.Registry
+	assigned []Chunk
 }
 
 // This does not assign partitions, much call Rebalance() to get assignment and
@@ -17,14 +25,16 @@ func New(group string, id string, registry registry.Registry) (Membership, error
 	_, err := newConsumerRegistry(group, id, registry)
 
 	m := Membership{
+		id,
 		group,
 		registry,
+		[]Chunk{},
 	}
 	return m, err
 }
 
 func (m *Membership) Assigned() []Chunk {
-	return []Chunk{}
+	return m.assigned
 }
 
 func (m *Membership) AddTopic(topic string) error {
@@ -32,10 +42,79 @@ func (m *Membership) AddTopic(topic string) error {
 	return m.registry.Create(p, []byte{}, false)
 }
 
+// Rebalance the partitions among the consumer group. If cannot reach zookeeper
+// an error is returned so should sleep and retry.
 func (m *Membership) Rebalance() error {
+	if err := m.clearAssigned(); err != nil {
+		return err
+	}
+
+	consumers, err := m.consumers()
+	if err != nil {
+		return err
+	}
+	index := sort.SearchStrings(consumers, m.id)
+
+	// TODO(AD) Hard code 15 partitions for now - this should be configurable
+	// per topic (with global default)
+	nAssigned := nPartitions / len(consumers)
+
+	var from uint32 = uint32(nAssigned*index + 1)
+	var to uint32 = uint32(nAssigned*(index+1) + 1)
+
+	topics, err := m.topics()
+	if err != nil {
+		return err
+	}
+	for _, topic := range topics {
+		for partition := from; partition != to; partition++ {
+			if err := m.assign(Chunk{topic, partition}); err != nil {
+				return err
+			}
+		}
+	}
+
 	return nil
 }
 
 func (m *Membership) RequiresRebalance() <-chan bool {
 	return make(chan bool)
+}
+
+func (m *Membership) clearAssigned() error {
+	for _, c := range m.assigned {
+		p := path.Join("/", "partition", m.group, c.Topic, strconv.Itoa(int(c.Partition)))
+		if err := m.registry.Delete(p); err != nil {
+			return err
+		}
+	}
+	m.assigned = []Chunk{}
+	return nil
+}
+
+func (m *Membership) topics() ([]string, error) {
+	topics, err := m.registry.GetRoot("/partition/" + m.group)
+	if err != nil {
+		return []string{}, err
+	}
+	sort.Strings(topics)
+	return topics, nil
+}
+
+func (m *Membership) consumers() ([]string, error) {
+	consumers, err := m.registry.GetRoot("/group/" + m.group)
+	if err != nil {
+		return []string{}, err
+	}
+	sort.Strings(consumers)
+	return consumers, nil
+}
+
+func (m *Membership) assign(c Chunk) error {
+	p := path.Join("/", "partition", m.group, c.Topic, strconv.Itoa(int(c.Partition)))
+	if err := m.registry.CreateErrIfExist(p, []byte(m.id), true); err != nil {
+		return err
+	}
+	m.assigned = append(m.assigned, c)
+	return nil
 }
--- client/pkg/consumer/membership/membership_test.go
+++ client/pkg/consumer/membership/membership_test.go
@@ -2,6 +2,10 @@ package membership
 
 import (
 	"fmt"
+	"path"
+	"reflect"
+	"sort"
+	"strconv"
 	"testing"
 
 	"github.com/dunstall/wombatclient/pkg/consumer/registry"
@@ -54,11 +58,202 @@ func TestNewIDAlreadyInUse(t *testing.T) {
 	}
 }
 
-// TODO(AD) Rebalance first
-// func TestInitialRebalance(t *testing.T) {
-// }
+func TestFirstRebalance(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	r := mock_registry.NewMockRegistry(ctrl)
+	// Ignore the node registering itself (tested above).
+	r.EXPECT().Create(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
 
-// TODO(AD) Rebalance not first
+	group := "mygroup"
+	id := "id-2"
+	m, err := New(group, id, r)
+	if err != nil {
+		t.Error(err)
+	}
+
+	topics := []string{"topic3", "topic2", "topic1"}
+	consumers := []string{"id-3", id, "id-1"}
+
+	r.EXPECT().GetRoot("/partition/"+group).Return(topics, nil)
+	r.EXPECT().GetRoot("/group/"+group).Return(consumers, nil)
+
+	// Should be assigned the middle third of partitions for each topic.
+	var expectedFrom uint32 = 6
+	var expectedTo uint32 = 11
+	expectedAssigned := []Chunk{}
+	sort.Strings(topics)
+	for _, t := range topics {
+		for p := expectedFrom; p != expectedTo; p++ {
+			expectedAssigned = append(expectedAssigned, Chunk{t, p})
+		}
+	}
+
+	for _, c := range expectedAssigned {
+		p := path.Join("/", "partition", group, c.Topic, strconv.Itoa(int(c.Partition)))
+		// Returns nil for success.
+		r.EXPECT().CreateErrIfExist(p, []byte(id), true).Return(nil)
+	}
+
+	if err = m.Rebalance(); err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(expectedAssigned, m.Assigned()) {
+		t.Errorf("m.Assigned() = %v, expected %v", m.Assigned(), expectedAssigned)
+	}
+}
+
+func TestRebalanceAfterTopicUpdate(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	r := mock_registry.NewMockRegistry(ctrl)
+	// Ignore the node registering itself (tested above).
+	r.EXPECT().Create(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
+
+	group := "mygroup"
+	id := "id-2"
+	m, err := New(group, id, r)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Just assign expected initial chunks - this is tested above.
+	topicsInitial := []string{"topic3", "topic2", "topic1"}
+	var from uint32 = 6
+	var to uint32 = 11
+	sort.Strings(topicsInitial)
+	initialAssigned := []Chunk{}
+	for _, t := range topicsInitial {
+		for p := from; p != to; p++ {
+			initialAssigned = append(initialAssigned, Chunk{t, p})
+		}
+	}
+	m.assigned = initialAssigned
+
+	// Expect the rebalance to first clear all assigned partitions.
+	for _, c := range initialAssigned {
+		// Returns nil for success.
+		p := "/partition/" + group + "/" + c.Topic + "/" + strconv.Itoa(int(c.Partition))
+		r.EXPECT().Delete(p).Return(nil)
+	}
+
+	topicsNew := []string{"topic3", "topic2", "topic1", "topic-new1", "topic-new2"}
+	consumers := []string{"id-3", id, "id-1"}
+
+	r.EXPECT().GetRoot("/partition/"+group).Return(topicsNew, nil)
+	r.EXPECT().GetRoot("/group/"+group).Return(consumers, nil)
+
+	var expectedFrom uint32 = 6
+	var expectedTo uint32 = 11
+	sort.Strings(topicsNew)
+	expectedAssigned := []Chunk{}
+	for _, t := range topicsNew {
+		for p := expectedFrom; p != expectedTo; p++ {
+			expectedAssigned = append(expectedAssigned, Chunk{t, p})
+		}
+	}
+
+	for _, c := range expectedAssigned {
+		// Returns nil for success.
+		p := "/partition/" + group + "/" + c.Topic + "/" + strconv.Itoa(int(c.Partition))
+		r.EXPECT().CreateErrIfExist(p, []byte(id), true).Return(nil)
+	}
+
+	if err = m.Rebalance(); err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(expectedAssigned, m.Assigned()) {
+		t.Errorf("m.Assigned() = %v, expected %v", m.Assigned(), expectedAssigned)
+	}
+}
+
+func TestRebalanceAfterConsumerUpdate(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	r := mock_registry.NewMockRegistry(ctrl)
+	// Ignore the node registering itself (tested above).
+	r.EXPECT().Create(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
+
+	group := "mygroup"
+	id := "id-2"
+	m, err := New(group, id, r)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Just assign expected initial chunks - this is tested above.
+	topicsInitial := []string{"topic3", "topic2", "topic1"}
+	var from uint32 = 6
+	var to uint32 = 11
+	sort.Strings(topicsInitial)
+	initialAssigned := []Chunk{}
+	for _, t := range topicsInitial {
+		for p := from; p != to; p++ {
+			initialAssigned = append(initialAssigned, Chunk{t, p})
+		}
+	}
+	m.assigned = initialAssigned
+
+	// Expect the rebalance to first clear all assigned partitions.
+	for _, c := range initialAssigned {
+		// Returns nil for success.
+		p := "/partition/" + group + "/" + c.Topic + "/" + strconv.Itoa(int(c.Partition))
+		r.EXPECT().Delete(p).Return(nil)
+	}
+
+	topics := []string{"topic3", "topic2", "topic1"}
+	consumersNew := []string{"id-3", id, "id-1", "id-new1", "id-new2"}
+
+	r.EXPECT().GetRoot("/partition/"+group).Return(topics, nil)
+	r.EXPECT().GetRoot("/group/"+group).Return(consumersNew, nil)
+
+	var expectedFrom uint32 = 4
+	var expectedTo uint32 = 7
+	sort.Strings(topics)
+	expectedAssigned := []Chunk{}
+	for _, t := range topics {
+		for p := expectedFrom; p != expectedTo; p++ {
+			expectedAssigned = append(expectedAssigned, Chunk{t, p})
+		}
+	}
+
+	for _, c := range expectedAssigned {
+		// Returns nil for success.
+		p := "/partition/" + group + "/" + c.Topic + "/" + strconv.Itoa(int(c.Partition))
+		r.EXPECT().CreateErrIfExist(p, []byte(id), true).Return(nil)
+	}
+
+	if err = m.Rebalance(); err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(expectedAssigned, m.Assigned()) {
+		t.Errorf("m.Assigned() = %v, expected %v", m.Assigned(), expectedAssigned)
+	}
+}
+
+func TestInitialRebalanceNoTopics(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	r := mock_registry.NewMockRegistry(ctrl)
+	r.EXPECT().Create(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
+
+	group := "mygroup"
+	m, err := New(group, "", r)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Return no topics.
+	r.EXPECT().GetRoot("/partition/"+group).Return([]string{}, nil)
+	r.EXPECT().GetRoot("/group/"+group).Return([]string{"a", "b", "c"}, nil)
+
+	if err = m.Rebalance(); err != nil {
+		t.Error(err)
+	}
+}
 
 func TestAddTopicOk(t *testing.T) {
 	ctrl := gomock.NewController(t)
