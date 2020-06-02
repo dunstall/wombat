c779aec016c612c9b7f7b3eaa80eb559f8cdb0e1
Andy Dunstall <andydunstall@hotmail.com>
2020-06-02 14:53:27 -0400

    Remove unused code

--- client/src/partition.rs
+++
@@ -1,6 +0,0 @@
-mod connection;
-mod loadbalancer;
-mod route;
-mod router;
-
-pub use router::Router;
--- core/src/lib.rs
+++
@@ -1,4 +0,0 @@
-mod packet;
-
-// TODO(AD) Rename consume*
-pub use packet::{ConsumeRequest, ConsumeResponse, Header, MessageError, ProduceRecord, Type};
