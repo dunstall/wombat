17c696555debb6ad86ed7d3089ac937a4accb35c
Andy Dunstall <andydunstall@hotmail.com>
2020-06-06 09:42:49 -0400

    Log benchmark

--- log/tests/log.rs
+++ log/tests/log.rs
@@ -1,9 +1,11 @@
 use wombatlog::{Log, SegmentManager, SystemSegmentManager};
 
 use rand::Rng;
+use std::time::Instant;
 use tempdir::TempDir;
 
 #[test]
+#[ignore]
 fn random_data() {
     let dir = tmp_dir();
 
@@ -15,17 +17,16 @@ fn random_data() {
     let mut rng = rand::thread_rng();
 
     for n in (0..100_000) {
-        let numbers: Vec<u8> = (0..0xff).map(|_| {
-            rng.gen_range(0, 0xff)
-        }).collect();
+        let numbers: Vec<u8> = (0..0xff).map(|_| rng.gen_range(0, 0xff)).collect();
 
         log.append(&numbers).unwrap();
-        assert_eq!(numbers, log.lookup(0xff, n*0xff).unwrap());
+        assert_eq!(numbers, log.lookup(0xff, n * 0xff).unwrap());
     }
 }
 
 #[test]
-fn large_log() {
+#[ignore]
+fn benchmark() {
     let dir = tmp_dir();
 
     let mut manager: std::boxed::Box<(dyn SegmentManager + 'static)> =
@@ -34,17 +35,18 @@ fn large_log() {
     let mut log = Log::open(&mut manager, 1_000_000_000).unwrap();
 
     let mut rng = rand::thread_rng();
-    let numbers: Vec<u8> = (0..0xff).map(|_| {
-        rng.gen_range(0, 0xff)
-    }).collect();
+    let numbers: Vec<u8> = (0..0xff).map(|_| rng.gen_range(0, 0xff)).collect();
 
-    for _ in (0..100_000_000) {
+    let now = Instant::now();
+    for _ in (0..10_000_000) {
         log.append(&numbers).unwrap();
     }
+    println!("Append time {}", now.elapsed().as_secs());
 
-    for n in (0..100_000_000) {
-        assert_eq!(numbers, log.lookup(0xff, n*0xff).unwrap());
+    for n in (0..10_000_000) {
+        assert_eq!(numbers, log.lookup(0xff, n * 0xff).unwrap());
     }
+    println!("Append and lookup time {}", now.elapsed().as_secs());
 }
 
 fn tmp_dir() -> TempDir {
