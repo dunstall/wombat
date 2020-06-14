d8bf13b2b5b1f7f9fc2c0d6ea5b160f3da400357
Andy Dunstall <andydunstall@hotmail.com>
2020-06-14 10:23:11 -0400

    Add log send to fd

--- log-cpp/include/log/log.h
+++ log-cpp/include/log/log.h
@@ -26,6 +26,13 @@ class Log {
     offsets_.Insert(0, active_);
   }
 
+  // TODO
+  // Log(const Log&) = delete;
+  // Log& operator=(const Log&) = delete;
+
+  // Log(Log&&) = default;
+  // Log& operator=(Log&&) = default;
+
   void Append(const std::vector<uint8_t>& data) {
     // Allow at() to throw as should never happen if the id is in offsets.
     S segment = segments_.at(active_);
@@ -44,18 +51,52 @@ class Log {
       // This should never happen a segment at offset 0 is always added.
       throw LogException("offset not found");
     }
+    return LookupSegment(id).Lookup(offset - starting_offset, size);
+  }
 
+  uint64_t Send(uint64_t offset, uint64_t size, int fd) {
+    uint64_t written = 0;
+    while (written < size) {
+      uint32_t id;
+      uint32_t starting_offset;
+      if (!offsets_.Lookup(offset, &id, &starting_offset)) {
+        // This should never happen a segment at offset 0 is always added.
+        throw LogException("offset not found");
+      }
+
+      S segment = LookupSegment(id);
+      uint64_t n = segment.Send(offset - starting_offset, size - written, fd);
+      // If reach EOF return the bytes already written.
+      if (n == 0) {
+        return written;
+      }
+
+      written += n;
+      offset += n;
+    }
+
+    return written;
+  }
+
+  uint64_t Recv(uint64_t size, int fd) {
+    // TODO(AD)
+    // Must ensure the segments on each replica are identical - this is so
+    // Lookup() never reaches EOF incorrectly (as record split among segments)
+    // and so segments can be transfered in parallel later.
+    return 0;
+  }
+
+ private:
+  S LookupSegment(uint64_t id) {
     // Lazily load the segments. TODO if too make open FDs is an issue could
     // only keep latest open (LRU cache).
     if (segments_.find(id) == segments_.end()) {
       segments_.emplace(id, S{id, path_, segment_limit_});
     }
-
     // Allow at() to throw as should never happen if the id is in offsets.
-    return segments_.at(id).Lookup(offset - starting_offset, size);
+    return segments_.at(id);
   }
 
- private:
   Offsets<S> offsets_;
 
   std::filesystem::path path_;
--- log-cpp/tests/log_unittest.cpp
+++ log-cpp/tests/log_unittest.cpp
@@ -1,3 +1,6 @@
+#include <fcntl.h>
+#include <sys/mman.h>
+
 #include <vector>
 
 #include "gtest/gtest.h"
@@ -45,6 +48,71 @@ TEST_F(LogTest, LookupEof) {
   Log<InMemorySegment> log{GeneratePath(), 3};
   log.Append({1, 2, 3, 4});
   EXPECT_TRUE(log.Lookup(200U, 3U).empty());
+}
+
+TEST_F(LogTest, SendFullLog) {
+  const auto path = GeneratePath();
+
+  Log<InMemorySegment> log{path, 3};
+  log.Append({1, 2, 3, 4});  // Segment 1.
+  log.Append({5, 6, 7, 8});  // Segment 2.
+
+  int fd = memfd_create("myfd", O_RDWR);
+  if (fd == -1) FAIL();
+
+  EXPECT_EQ(8U, log.Send(0, 8, fd));
+
+  if (lseek(fd, 0, SEEK_SET) != 0) FAIL();
+  uint8_t buf[8];
+  if (read(fd, buf, 8) != 8) FAIL();
+
+  std::vector<uint8_t> expected{1, 2, 3, 4, 5, 6, 7, 8};
+  std::vector<uint8_t> read(buf, buf + 8);
+  EXPECT_EQ(expected, read);
+}
+
+TEST_F(LogTest, SendAccrossMultiSegment) {
+  const auto path = GeneratePath();
+
+  Log<InMemorySegment> log{path, 3};
+  log.Append({1, 2, 3, 4});  // Segment 1.
+  log.Append({5, 6, 7, 8});  // Segment 2.
+  log.Append({9, 10});  // Segment 3.
+  log.Append({11, 12});  // Segment 3.
+
+  int fd = memfd_create("myfd", O_RDWR);
+  if (fd == -1) FAIL();
+
+  EXPECT_EQ(8U, log.Send(2, 8, fd));
+
+  if (lseek(fd, 0, SEEK_SET) != 0) FAIL();
+  uint8_t buf[8];
+  if (read(fd, buf, 8) != 8) FAIL();
+
+  std::vector<uint8_t> expected{3, 4, 5, 6, 7, 8, 9, 10};
+  std::vector<uint8_t> read(buf, buf + 8);
+  EXPECT_EQ(expected, read);
+}
+
+TEST_F(LogTest, SendOverflow) {
+  const auto path = GeneratePath();
+
+  Log<InMemorySegment> log{path, 3};
+  log.Append({1, 2, 3, 4});  // Segment 1.
+  log.Append({5, 6, 7, 8});  // Segment 2.
+
+  int fd = memfd_create("myfd", O_RDWR);
+  if (fd == -1) FAIL();
+
+  EXPECT_EQ(8U, log.Send(0, 20, fd));
+
+  if (lseek(fd, 0, SEEK_SET) != 0) FAIL();
+  uint8_t buf[8];
+  if (read(fd, buf, 8) != 8) FAIL();
+
+  std::vector<uint8_t> expected{1, 2, 3, 4, 5, 6, 7, 8};
+  std::vector<uint8_t> read(buf, buf + 8);
+  EXPECT_EQ(expected, read);
 }
 
 }  // namespace wombat::log::testing
