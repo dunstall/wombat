daf2dbffd67b8a7b6f44e3c60294ff520af8e8ea
Andy Dunstall <andydunstall@hotmail.com>
2020-06-27 10:11:43 -0400

    Fix broker static analysis checks

--- broker/src/main.cpp
+++ broker/src/main.cpp
@@ -1,10 +1,12 @@
+// Copyright 2020 Andrew Dunstall
+
 #include <chrono>
 #include <cstdlib>
 #include <iostream>
 #include <thread>
 #include <vector>
 
-#include <glog/logging.h>
+#include "glog/logging.h"
 #include "log/log.h"
 #include "log/systemsegment.h"
 #include "log/tempdir.h"
@@ -12,6 +14,41 @@
 #include "partition/replica.h"
 #include "record/producerecord.h"
 #include "server/server.h"
+
+namespace wombat::broker {
+
+void RunLeader() {
+  TempDir dir{};
+  std::shared_ptr<Log<SystemSegment>> log
+      = std::make_shared<Log<SystemSegment>>(dir.path(), 128'000'000);
+  Leader<SystemSegment> leader{log, 3110};
+
+  server::Server server{3111};
+
+  while (true) {
+    std::vector<ProduceRecord> requests = server.Poll();
+    for (const auto r : requests) {
+      log->Append(r.Encode());
+    }
+
+    leader.Poll();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+  }
+}
+
+void RunReplica() {
+  TempDir dir{};
+  std::shared_ptr<Log<SystemSegment>> log
+      = std::make_shared<Log<SystemSegment>>(dir.path(), 128'000'000);
+  Replica<SystemSegment> replica{log, {"127.0.0.1", 3110}};
+
+  while (true) {
+    replica.Poll();
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+  }
+}
+
+}  // namespace wombat::broker
 
 void PrintUsage() {
   std::cout << "USAGE:" << std::endl;
@@ -30,38 +67,10 @@ int main(int argc, char** argv) {
 
   if (std::string(argv[1]) == "leader") {
     std::cout << "leader" << std::endl;
-
-    wombat::broker::TempDir dir{};
-    std::shared_ptr<wombat::broker::Log<wombat::broker::SystemSegment>> log
-        = std::make_shared<wombat::broker::Log<wombat::broker::SystemSegment>>(dir.path(), 128'000'000);
-    wombat::broker::Leader<wombat::broker::SystemSegment> leader{log, 3110};
-
-    wombat::broker::server::Server server{3111};
-
-    while (true) {
-      std::vector<wombat::broker::ProduceRecord> requests = server.Poll();
-      for (const auto r : requests) {
-        log->Append(r.Encode());
-      }
-
-      leader.Poll();
-      std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    }
-
-    // TODO(AD) run server
+    wombat::broker::RunLeader();
   } else if (std::string(argv[1]) == "replica" && argc == 3) {
     std::cout << "replica with leader address " << argv[2] << std::endl;
-     wombat::broker::TempDir dir{};
-    std::shared_ptr<wombat::broker::Log<wombat::broker::SystemSegment>> log
-        = std::make_shared<wombat::broker::Log<wombat::broker::SystemSegment>>(dir.path(), 128'000'000);
-    wombat::broker::Replica<wombat::broker::SystemSegment> replica{log, {"127.0.0.1", 3110}};
-
-    while (true) {
-      replica.Poll();
-      std::this_thread::sleep_for(std::chrono::milliseconds(50));
-    }
-
-    // TODO(AD) run server
+    wombat::broker::RunReplica();
   } else {
     PrintUsage();
     std::exit(EXIT_FAILURE);
