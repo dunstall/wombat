2f7d0a129b4bbbc43b920dea0267dea07f54d2f2
Andy Dunstall <andydunstall@hotmail.com>
2020-07-04 08:24:55 -0400

    Add response type

--- record/include/record/response.h
+++ record/include/record/response.h
@@ -9,12 +9,33 @@
 namespace wombat::broker::record {
 
 enum class ResponseType : uint32_t {
-  kConsumeResponse,
+  kProduce,
+  kConsume
 };
 
 class Response {
  public:
-  Response(ResponseType type, const std::vector<uint8_t>& payload) {}
+  Response(ResponseType type, const std::vector<uint8_t>& payload);
+
+  bool operator==(const Response& response) const;
+
+  bool operator!=(const Response& response) const;
+
+  ResponseType type() const { return type_; }
+
+  std::vector<uint8_t> payload() const { return payload_; }
+
+  std::vector<uint8_t> Encode() const;
+
+  static std::optional<Response> Decode(const std::vector<uint8_t>& enc);
+
+ private:
+  // Maximum record data size.
+  static constexpr uint32_t kLimit = 512;
+
+  ResponseType type_;
+
+  std::vector<uint8_t> payload_;
 };
 
 }  // namespace wombat::broker::record
