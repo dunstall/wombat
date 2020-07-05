b1c40bf593807042a4fea5c9b2135f3f0859ff1f
Andy Dunstall <andydunstall@hotmail.com>
2020-07-05 07:43:11 -0400

    Remove partition dependencies

--- partition/include/partition/partition.h
+++ partition/include/partition/partition.h
@@ -6,71 +6,24 @@
 #include <memory>
 #include <vector>
 
-#include "glog/logging.h"
-#include "log/log.h"
-#include "record/record.h"
-#include "record/recordrequest.h"
 #include "record/request.h"
 #include "record/response.h"
+#include "tmp/log.h"
 
 namespace wombat::broker::partition {
 
-template<class S>
 class Partition {
  public:
-  explicit Partition(std::shared_ptr<log::Log<S>> log) : log_{log} {}
+  explicit Partition(std::shared_ptr<Log> log);
 
-  std::optional<record::Response> Handle(const record::Request& request) {
-    switch (request.type()) {
-      case record::RequestType::kProduce:
-        Produce(request);
-        break;
-      case record::RequestType::kConsume:
-        return Consume(request);
-      default:
-        LOG(WARNING) << "unrecognized request type";
-    }
-    return std::nullopt;
-  }
+  std::optional<record::Response> Handle(const record::Request& request);
 
  private:
-  void Produce(const record::Request& request) {
-    // Decode the record to ensure only valid records are appended.
-    std::optional<record::Record> record
-        = record::Record::Decode(request.payload());
-    if (record) {
-      log_->Append(record->Encode());
-    } else {
-      LOG(WARNING) << "received invalid produce record";
-    }
-  }
+  void Produce(const record::Request& request);
 
-  std::optional<record::Response> Consume(const record::Request& request) {
-    std::optional<record::RecordRequest> cr
-        = record::RecordRequest::Decode(request.payload());
-    if (!cr) {
-      // If invalid request return nothing.
-      return std::nullopt;
-    }
+  std::optional<record::Response> Consume(const record::Request& request);
 
-    std::optional<uint32_t> size = record::DecodeU32(
-        log_->Lookup(cr->offset(), sizeof(uint32_t))
-    );
-    if (!size) {
-      // If the offset is not found return empty record.
-      return record::Response{
-        record::ResponseType::kConsume, record::Record{}.Encode()
-      };
-    }
-
-    // Since the records must be valid this will return size.
-    const std::vector<uint8_t> data = log_->Lookup(
-        cr->offset(), sizeof(uint32_t) + *size
-    );
-    return record::Response{record::ResponseType::kConsume, data};
-  }
-
-  std::shared_ptr<log::Log<S>> log_;
+  std::shared_ptr<Log> log_;
 };
 
 }  // namespace wombat::broker::partition
--- partition/tests/unit/partition_test.cpp
+++ partition/tests/unit/partition_test.cpp
@@ -3,25 +3,28 @@
 #include <memory>
 #include <vector>
 
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "partition/partition.h"
-#include "log/log.h"
-#include "log/inmemorysegment.h"
-#include "log/tempdir.h"
 #include "record/record.h"
 #include "record/recordrequest.h"
 #include "record/request.h"
+#include "tmp/log.h"
 
 namespace wombat::broker::partition::testing {
+
+class MockLog : public Log {
+ public:
+  MOCK_METHOD(void, Append, (const std::vector<uint8_t>& data), (override));
+  MOCK_METHOD(std::vector<uint8_t>, Lookup, (uint32_t offset, uint32_t size), (override));  // NOLINT
+  MOCK_METHOD(uint32_t, Send, (uint32_t offset, uint32_t size, int fd), (override));  // NOLINT
+};
 
 class PartitionTest : public ::testing::Test {};
 
 TEST_F(PartitionTest, HandleProduceOk) {
-  std::shared_ptr<log::Log<log::InMemorySegment>> log
-      = std::make_shared<log::Log<log::InMemorySegment>>(
-          log::GeneratePath(), 10'000
-      );
-  Partition<log::InMemorySegment> partition{log};
+  std::shared_ptr<MockLog> log = std::make_shared<MockLog>();
+  Partition partition{log};
 
   const std::vector<uint8_t> data{1, 2, 3, 4, 5};
   const record::Record record{data};
@@ -29,86 +32,72 @@ TEST_F(PartitionTest, HandleProduceOk) {
       record::RequestType::kProduce, record.Encode()
   };
 
-  EXPECT_EQ(std::nullopt, partition.Handle(request));
+  EXPECT_CALL(*log, Append(record.Encode())).Times(1);
 
-  EXPECT_EQ(record.Encode(), log->Lookup(0, record.Encode().size()));
+  EXPECT_EQ(std::nullopt, partition.Handle(request));
 }
 
 TEST_F(PartitionTest, HandleProduceInvalid) {
-  std::shared_ptr<log::Log<log::InMemorySegment>> log
-      = std::make_shared<log::Log<log::InMemorySegment>>(
-          log::GeneratePath(), 10'000
-      );
-  Partition<log::InMemorySegment> partition{log};
+  // No calls should be made to log.
+  std::shared_ptr<MockLog> log = std::make_shared<MockLog>();
+  Partition partition{log};
 
   const std::vector<uint8_t> payload{0, 0, 0xff, 0xff, 1, 2, 3};
   const record::Request request{record::RequestType::kProduce, payload};
 
   EXPECT_EQ(std::nullopt, partition.Handle(request));
-
-  // Log should still be empty.
-  EXPECT_EQ(0U, log->size());
-  EXPECT_TRUE(log->Lookup(0U, 5U).empty());
 }
 
 TEST_F(PartitionTest, HandleRecordRequestOk) {
-  std::shared_ptr<log::Log<log::InMemorySegment>> log
-      = std::make_shared<log::Log<log::InMemorySegment>>(
-          log::GeneratePath(), 10'000
-      );
-  Partition<log::InMemorySegment> partition{log};
-
-  // Add buffer data to read from non-zero offset.
-  log->Append({0, 0, 0, 0, 0});
-
+  std::shared_ptr<MockLog> log = std::make_shared<MockLog>();
+  Partition partition{log};
+  
   const std::vector<uint8_t> data{1, 2, 3, 4, 5};
   const record::Record record{data};
-  log->Append(record.Encode());
+  const std::vector<uint8_t> record_enc = record.Encode();
 
   const uint32_t offset = 0x05;
   const record::RecordRequest consume{offset};
   const record::Request request{
     record::RequestType::kConsume, consume.Encode()
   };
+  EXPECT_CALL(*log, Lookup(offset, sizeof(uint32_t)))
+      .WillOnce(::testing::Return(
+          std::vector<uint8_t>(record_enc.begin(), record_enc.begin() + 4)
+      ));
+  EXPECT_CALL(*log, Lookup(offset, record_enc.size()))
+      .WillOnce(::testing::Return(record_enc));
 
   std::optional<record::Response> response = partition.Handle(request);
-
   EXPECT_TRUE(response);
-  EXPECT_EQ(response->payload(), record.Encode());
+  EXPECT_EQ(response->payload(), record_enc);
 }
 
 TEST_F(PartitionTest, HandleRecordRequestOffsetNotFound) {
-  std::shared_ptr<log::Log<log::InMemorySegment>> log
-      = std::make_shared<log::Log<log::InMemorySegment>>(
-          log::GeneratePath(), 10'000
-      );
-  Partition<log::InMemorySegment> partition{log};
-
-  // Add buffer data to read from non-zero offset.
-  log->Append({0, 0, 0, 0, 0});
+  std::shared_ptr<MockLog> log = std::make_shared<MockLog>();
+  Partition partition{log};
 
   const uint32_t offset = 0x05;
   const record::RecordRequest consume{offset};
   const record::Request request{
     record::RequestType::kConsume, consume.Encode()
   };
+  EXPECT_CALL(*log, Lookup(offset, sizeof(uint32_t)))
+      .WillOnce(::testing::Return(std::vector<uint8_t>()));
 
+  // Empty record should be returned.
+  const record::Record record{{}};
+  const std::vector<uint8_t> record_enc = record.Encode();
+  
   std::optional<record::Response> response = partition.Handle(request);
-
-
-  const record::Record expected{{}};
-
   EXPECT_TRUE(response);
-  EXPECT_EQ(response->payload(), expected.Encode());
+  EXPECT_EQ(response->payload(), record_enc);
 }
 
 TEST_F(PartitionTest, HandleRecordRequestInvalid) {
-  std::shared_ptr<log::Log<log::InMemorySegment>> log
-      = std::make_shared<log::Log<log::InMemorySegment>>(
-          log::GeneratePath(), 10'000
-      );
-  Partition<log::InMemorySegment> partition{log};
-
+  std::shared_ptr<MockLog> log = std::make_shared<MockLog>();
+  Partition partition{log};
+  
   const record::Request request{record::RequestType::kConsume, {0, 0xff}};
 
   std::optional<record::Response> response = partition.Handle(request);
@@ -116,11 +105,8 @@ TEST_F(PartitionTest, HandleRecordRequestInvalid) {
 }
 
 TEST_F(PartitionTest, HandleUnrecognizedRecord) {
-  std::shared_ptr<log::Log<log::InMemorySegment>> log
-      = std::make_shared<log::Log<log::InMemorySegment>>(
-          log::GeneratePath(), 10'000
-      );
-  Partition<log::InMemorySegment> partition{log};
+  std::shared_ptr<MockLog> log = std::make_shared<MockLog>();
+  Partition partition{log};
 
   const std::vector<uint8_t> payload{1, 2, 3};
   const record::Request request{
@@ -128,10 +114,6 @@ TEST_F(PartitionTest, HandleUnrecognizedRecord) {
   };
 
   EXPECT_EQ(std::nullopt, partition.Handle(request));
-
-  // Log should still be empty.
-  EXPECT_EQ(0U, log->size());
-  EXPECT_TRUE(log->Lookup(0U, 5U).empty());
 }
 
 }  // namespace wombat::broker::partition::testing
