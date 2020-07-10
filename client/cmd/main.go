0838582e5b6d2e257d49fe90678f376798cac339
Andy Dunstall <andydunstall@hotmail.com>
2020-07-10 01:07:51 -0400

    Add record to go client

--- client/cmd/main.go
+++ client/cmd/main.go
@@ -2,40 +2,84 @@ package main
 
 import (
 	"encoding/binary"
-  "fmt"
-  "net"
+	"fmt"
+	"net"
 )
 
-type ProduceRecord struct {
-  data []byte
+const (
+	ProduceRequest = iota
+	ConsumeRequest
+	kReplicaRequest
+	kConsumeResponse
+	kReplicaResponse
+)
+
+type MessageHeader struct {
+	Kind        uint32
+	PartitionID uint32
+	PayloadSize uint32
 }
 
-func NewProduceRecord(data []byte) ProduceRecord {
-  return ProduceRecord{data}
+func (h *MessageHeader) Encode() []byte {
+	b := make([]byte, 12)
+	binary.BigEndian.PutUint32(b[:4], h.Kind)
+	binary.BigEndian.PutUint32(b[4:8], h.PartitionID)
+	binary.BigEndian.PutUint32(b[8:12], h.PayloadSize)
+	return b
 }
 
-func (r *ProduceRecord) Encode() []byte {
+type Message struct {
+	Header  MessageHeader
+	Payload []byte
+}
+
+func (m *Message) Encode() []byte {
+	b := m.Header.Encode()
+	b = append(b, m.Payload...)
+	return b
+}
+
+type Record struct {
+	data []byte
+}
+
+func NewRecord(data []byte) Record {
+	return Record{data}
+}
+
+func (r *Record) Encode() []byte {
 	b := make([]byte, 4)
 	binary.BigEndian.PutUint32(b, uint32(len(r.data)))
 	b = append(b, r.data...)
-  return b
+	return b
 }
 
 func main() {
-  conn, err := net.Dial("tcp", "localhost:3111")
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  defer conn.Close()
+	conn, err := net.Dial("tcp", "localhost:3110")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
 
-  for {
-    r := NewProduceRecord([]byte("testdata"))
-    n, err := conn.Write(r.Encode())
-    if err != nil {
-      fmt.Println(err)
-      return
-    }
-    fmt.Println("write", n)
-  }
+	for {
+		r := NewRecord([]byte("testdata"))
+
+		h := MessageHeader{
+			Kind:        ProduceRequest,
+			PartitionID: 0,
+			PayloadSize: uint32(len(r.Encode())),
+		}
+		m := Message{
+			Header:  h,
+			Payload: r.Encode(),
+		}
+
+		n, err := conn.Write(m.Encode())
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("write", n)
+	}
 }
