6c1bfcc331f854975f6724d67e89ac17f047501d
Andy Dunstall <andydunstall@hotmail.com>
2020-07-12 06:33:05 -0400

    Ignore line length in static analysis

--- bin/analize.sh
+++ bin/analize.sh
@@ -2,4 +2,4 @@
 
 # Requires cpplint (pip3 install cpplint)
 
-cpplint --recursive --exclude=lib/ --filter=-build/c++11,-whitespace/parens .
+cpplint --recursive --exclude=lib/ --filter=-build/c++11,-whitespace/parens,-whitespace/line_length .
