1141dbc87165d9c47eff19a0099a9cbd077dbeec
Andy Dunstall <andydunstall@hotmail.com>
2020-07-16 01:02:40 -0400

    Move broker to subdirectory

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-GLOG_logtostderr=1 ./bazel-bin/broker/wombat-broker broker/tests/data/WOMBATLEADER.conf
+GLOG_logtostderr=1 ./bazel-bin/broker/wombat-broker broker/broker/tests/data/WOMBATLEADER.conf
